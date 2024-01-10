00d562487fd00109b5c81fd2f69fc6986154497c
j_wiltfang <wiltfang.jasper@gmail.com>
2024-01-10 17:12:54 +0100

    fix loading of feedback POIs

--- public/src/feedback.js
+++ public/src/feedback.js
@@ -11,24 +11,38 @@ function callFeedback(callback) {
         })
         .then(data => {
             // Process retrieved documents
-            console.log(data)
-            const transformedData = data.documents.map(doc => {
-                const documentFields = doc.fields;
-                return {
-                    date: documentFields.date.stringValue,
-                    category: documentFields.category.stringValue,
-                    location: {
-                        lat: documentFields.location.geoPointValue.latitude,
-                        lon: documentFields.location.geoPointValue.longitude
-                    },
-                    photos: documentFields.photos.arrayValue.values.map(photo => photo.stringValue),
-                    description: documentFields.description.stringValue
-                };
-            });
+            console.log(data);
+            
+            if (data.documents && Array.isArray(data.documents)) {
+                const transformedData = data.documents.map(doc => {
+                    const documentFields = doc.fields;
 
-            // Invoke the callback function with the transformed data
-            console.log(transformedData)
-            callback(transformedData);
+                    // Ensure that 'documentFields.photos.arrayValue.values' exists and is an array
+                    const photos = documentFields.photos &&
+                        documentFields.photos.arrayValue &&
+                        documentFields.photos.arrayValue.values &&
+                        Array.isArray(documentFields.photos.arrayValue.values)
+                        ? documentFields.photos.arrayValue.values.map(photo => photo.stringValue)
+                        : [];
+
+                    return {
+                        date: documentFields.date.stringValue || '',
+                        category: documentFields.category.stringValue || '',
+                        location: {
+                            lat: documentFields.location.geoPointValue.latitude || 0,
+                            lon: documentFields.location.geoPointValue.longitude || 0
+                        },
+                        photos,
+                        description: documentFields.description.stringValue || ''
+                    };
+                });
+
+                // Invoke the callback function with the transformed data
+                console.log(transformedData);
+                callback(transformedData);
+            } else {
+                console.error('Invalid data structure received.');
+            }
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
